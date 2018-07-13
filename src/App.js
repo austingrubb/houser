0e7056dc745c722dc60eb2cc92079ab40a2634c1
austin <austin.grubb.1995@gmail.com>
2018-07-13 14:04:40 -0700

    first

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import Dashboard from './component/Dashboard/Dashboard';
+import Wizard from './component/Wizard/Wizard';
+import Header from './component/Header/Header';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <Wizard/>
+        <Dashboard/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/component/Dashboard/Dashboard.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import Header from '../Header/Header'
+
+class Dashboard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+          
+         }
+      }
+    render(){   
+        return(
+            <div>
+                <Header/>
+            </div>
+        )
+    }
+}
+
+export default Dashboard;
